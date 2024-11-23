de5e9633d3f9d1022d338fa76cbf4a34dbdedc48
Talisson Oliveira <talissonlux@outlook.com>
2024-11-23 04:26:50 -0300

    feat: create the design system with storybook and turborepo

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,28 @@
+import { Text, TextProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto pariatur porro ducimus autem, iste, minus sed recusandae perspiciatis aspernatur sapiente in quae. Necessitatibus porro quam, fugiat nemo laudantium assumenda exercitationem?',
+  },
+  argTypes: {
+    size: {
+      options: ['xxs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '4xl', '5xl', '6xl', '7xl', '8xl', '9xl'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
